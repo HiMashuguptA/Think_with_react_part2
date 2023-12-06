41a890ae2f039391cce56184f03636b859c10b90
HiMashuguptA <sg707012@gmail.com>
2023-12-06 18:19:57 +0530

    Final

---
+++ app.js
@@ -0,0 +1,20 @@
+// class ReactContainer extends React.Component{
+//     render(){
+//         return (
+//         <div>
+//             Hello! Welcome to Kalvium 
+//             <div>Lets's rock and roll, This is babel</div>
+//         </div>
+//         )
+//     }
+// }
+function ReactContainer(){
+    return (
+                <div>
+                    Hello! Welcome to Kalvium 
+                    <div>Lets's rock and roll, This is babel</div>
+                </div>
+            )
+}
+let container = document.getElementById("react-container")
+ReactDOM.render(< ReactContainer />, container)
